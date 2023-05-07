32c41addd41f2c47e0a1cb86ae9fc6a86a61be5c
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-05-08 02:27:01 +0330

    add user_level in profile

---
+++ app/Http/Livewire/Client/Profile/UserLevel/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Client\Profile\UserLevel;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.client.profile.user-level.index')->layout('layouts.app');
+    }
+}
