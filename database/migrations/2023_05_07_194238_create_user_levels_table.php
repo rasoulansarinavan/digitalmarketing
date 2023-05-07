f3a4de138560883dea16ff224bf26c6c5f845615
LeGion <mehrdad.dadashi@gmail.com>
2023-05-07 23:07:09 +0300

    refactor

--- database/migrations/2023_05_07_194238_create_user_levels_table.php
+++ database/migrations/2023_05_07_194238_create_user_levels_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
             $table->string('title');
             $table->text('description');
             $table->text('icon');
+
             $table->timestamps();
         });
     }
