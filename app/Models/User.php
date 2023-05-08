944ac7a3dfec42bcdea350ca3dd98331f2acc99a
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-05-08 22:59:25 +0330

    file upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,15 +4,78 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 use Laravel\Sanctum\HasApiTokens;
+use Livewire\WithFileUploads;
+
 use Spatie\Permission\Traits\HasPermissions;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
+
 {
+    use HasFactory;
+    use WithFileUploads;
+
+    protected $guarded = [];
+
+    public function saveUser($formData, $user_id, $image)
+    {
+        DB::transaction(function () use ($image, $user_id, $formData) {
+            $image_name = '';
+            if ($image) {
+                $extension = $image->extension();
+                $image_name = 'user_' . str::random(10) . time() . '.' . $extension;
+                if ($user_id != 0) {
+                    $userFileName = File::query()->where([
+                        'product_id' => $user_id,
+                        'type' => 'user',
+                    ])->pluck('file')->first();
+                    unlink('images/users/' . $userFileName);
+                }
+                Image::make($image)->crop('300', '300')->save(public_path('images/users/' . $image_name));
+            }
+
+            $user = $this->insertToUserTable($user_id, $formData);
+            if ($image) {
+                $this->insertToFileTable($user->id, $image_name);
+            }
+        });
+    }
+
+    public function insertToUserTable($user_id, $formData)
+    {
+        return User::query()->updateOrCreate(
+            [
+                'id' => $user_id
+            ],
+            [
+                'code_melli' => $formData['code_melli'],
+                'birth_date' => $formData['birth_date']
+            ]
+        );
+    }
+
+    public function insertToFileTable($user_id, $image_name)
+    {
+        File::query()->updateOrCreate(
+            [
+                'product_id' => $user_id,
+                'type' => 'user'
+            ],
+            [
+                'file' => $image_name
+            ]
+        );
+
+    }
+
     use HasApiTokens, HasFactory, Notifiable, HasPermissions, HasRoles;
 
     /**
--- resources/views/livewire/client/profile/user-level/index.blade.php
+++ resources/views/livewire/client/profile/user-level/index.blade.php
@@ -20,9 +20,9 @@
                                         <div class="settings-block-body">
                                             <div class="pricing-chnage-plan">
                                                 @foreach($levels as $level)
-                                                    <a
-                                                       class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12"
-                                                       style="max-width: inherit;" href="{{route('profile.kyc',$level->id)}}">
+                                                    <a class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12"
+                                                       style="max-width: inherit;"
+                                                       href="{{route('profile.kyc',$level->id)}}">
                                                         <h5>{{$level->title}}</h5>
                                                         <h6>{{$level->short_description}}</h6>
                                                     </a>
@@ -32,55 +32,72 @@
                                     </div>
                                 </div>
                             </div>
+
                             <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
-                                <div class="row gutters">
-                                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
-                                        <img src="/backend/img/user1.png" class="img-fluid change-img-avatar"
-                                             alt="Image">
-                                    </div>
-                                    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-12">
-                                        <div id="dropzone-sm" class="mb-4">
-                                            <form action="/upload" class="dropzone needsclick dz-clickable"
-                                                  id="demo-upload">
-
-                                                <div class="dz-message needsclick">
-                                                    <button type="button" class="dz-button">تصویر کارت ملی</button>
-                                                </div>
-
-                                            </form>
+                                <form wire:submit.prevent="saveUser(Object.fromEntries(new FormData($event.target)))">
+                                    <div class="row gutters">
+                                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4 col-12">
+                                            <img src="/backend/img/user1.png" class="img-fluid change-img-avatar"
+                                                 alt="Image">
+                                        </div>
+                                        <div
+                                            class="col-xl-8 col-lg-8 col-md-8 col-sm-8 col-12">
+                                            <div id="dropzone-sm" class="mb-4 dropzone needsclick dz-clickable">
+                                                <label class="form-label" for="image">تصویر کارت ملی</label>
+                                                <input wire:model="image"
+                                                       class="form-control @error('image') error-input-border @enderror"
+                                                       id="image" type="file">
+                                                @foreach ($errors->get('image') as $message)
+                                                    <span wire:loading.remove
+                                                          class=" text-danger w-100 d-block mt-2">{{ $message}}</span>
+                                                @endforeach
+                                                <div wire:loading wire:target="image">Uploading...</div>
+                                            </div>
+                                        </div>
+                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                            <!-- Field wrapper start -->
+                                            <div class="field-wrapper">
+                                                <input wire:model.defer="code_melli" name="code_melli"
+                                                       id="code_melli" type="text"
+                                                       class="form-control @error('code_melli') error-input-border @enderror"
+                                                       placeholder="اینجا تایپ کنید">
+                                                @foreach ($errors->get('code_melli') as $message)
+                                                    <span wire:loading.remove
+                                                          class=" text-danger w-100 d-block mt-2">{{ $message}}</span>
+                                                @endforeach
+                                                <div class="field-placeholder">شماره کد ملی</div>
+                                            </div>
+                                            <!-- Field wrapper end -->
+                                        </div>
+                                        <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
+                                            <!-- Field wrapper start -->
+                                            <div class="field-wrapper">
+                                                <input wire:model.defer="birth_date" name="birth_date"
+                                                       id="birth_date" type="date"
+                                                       class="form-control @error('birth_date') error-input-border @enderror">
+                                                @foreach ($errors->get('birth_date') as $message)
+                                                    <span wire:loading.remove
+                                                          class=" text-danger w-100 d-block mt-2">{{ $message}}</span>
+                                                @endforeach
+                                                <div class="field-placeholder">تاریخ تولد</div>
+                                            </div>
+                                            <!-- Field wrapper end -->
                                         </div>
                                     </div>
-                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                        <!-- Field wrapper start -->
-                                        <div class="field-wrapper">
-                                            <input type="text" class="form-control" placeholder="اینجا تایپ کنید">
-                                            <div class="field-placeholder">شماره کد ملی</div>
-                                        </div>
-                                        <!-- Field wrapper end -->
+                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <button type="submit" class="btn btn-primary mb-3">ذخیره اطلاعات</button>
                                     </div>
-                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12">
-                                        <!-- Field wrapper start -->
-                                        <div class="field-wrapper">
-                                            <input type="date" class="form-control">
-                                            <div class="field-placeholder">تاریخ تولد</div>
-                                        </div>
-                                        <!-- Field wrapper end -->
-                                    </div>
-                                </div>
-                                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                    <button class="btn btn-primary mb-3">ذخیره اطلاعات</button>
-                                </div>
+                                </form>
                             </div>
                             <div class="col-xl-3 col-lg-3 col-md-12 col-sm-12 col-12">
                                 <div class="account-settings-block">
-
                                     <div class="settings-block">
                                         <div class="settings-block-title"><b>نکات مهم این سطح :</b></div>
                                         <div class="settings-block-body">
                                             <div class="list-group">
-                                                    <div>
-                                                        {{$levelInfo->long_description}}
-                                                    </div>
+                                                <div>
+                                                    {{$levelInfo->long_description}}
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
