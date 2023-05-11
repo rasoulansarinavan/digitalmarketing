8271cc2b9045dbbc85e0fdf8e73158ea1410ad1c
LeGion <mehrdad.dadashi@gmail.com>
2023-05-12 02:42:57 +0300

    refactor level 1 view

---
+++ database/migrations/2023_05_11_201604_create_kycs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kycs', function (Blueprint $table) {
+            $table->id();
+            $table->text('data');
+            $table->foreignId('user_level_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('status_id')->default(1)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kycs');
+    }
+};
---
+++ database/seeders/StatusesTableSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class StatusesTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('statuses')->delete();
+        
+        \DB::table('statuses')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'title' => 'در حال بررسی',
+                'type' => 'kyc',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'title' => 'تایید شده',
+                'type' => 'kyc',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'title' => 'تایید نشده',
+                'type' => 'kyc',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+        ));
+        
+        
+    }
+}
