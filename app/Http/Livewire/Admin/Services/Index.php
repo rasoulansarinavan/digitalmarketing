18ac6dd782bb55c05e2508b7bff474a4aaf44d26
rasoulansarinavan <rasoulansarinavan98@gmail.com>
2023-05-16 23:41:51 +0330

    add delete in index component services

--- app/Http/Livewire/Admin/Services/Index.php
+++ app/Http/Livewire/Admin/Services/Index.php
@@ -4,11 +4,22 @@ namespace App\Http\Livewire\Admin\Services;
 
 use App\Models\File;
 use App\Models\Service;
+use App\Models\ServiceSeoItems;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Index extends Component
 {
+
+    public function deleteService($id)
+    {
+
+        $fileName = File::query()->where(['service_id' => $id, 'type' => 'service'])->pluck('file')->first();
+        unlink($fileName);
+        return  Service::query()->where('id', $id)->delete();
+    }
+
     public function render()
     {
         $services = Service::all();
